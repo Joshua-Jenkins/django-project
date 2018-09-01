cc4d34cd1a762274f7cb9ffa7a7f7d71e5217bea
Joshua-Jenkins <joshjenkins6@yahoo.com>
2018-09-01 14:56:12 -0500

    added templates and under views added a render to view the html files from blog templetes

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # added BlogConfig from blog/apps.py file
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
