987bc487f3795fc6a3a6023c24da37e77b10b0ea
Joshua-Jenkins <joshjenkins6@yahoo.com>
2018-09-01 15:05:29 -0500

    added a forloop to display the post on the home page

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-
-
+# FAKE DATA
+posts = [
+    {
+        'author': 'Joshua Jenkins',
+        'title': 'blog post one',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Fred Doe',
+        'title': 'blog post two',
+        'content': 'First post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
