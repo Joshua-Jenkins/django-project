53512a21413b11481717d5f6c765bb68002ba456
Joshua-Jenkins <joshjenkins6@yahoo.com>
2018-09-02 01:55:08 -0500

    added data to the admin site

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
-# FAKE DATA
-posts = [
-    {
-        'author': 'Joshua Jenkins',
-        'title': 'blog post one',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Fred Doe',
-        'title': 'blog post two',
-        'content': 'First post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
+
+
+
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
